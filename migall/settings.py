c180b3c3c20617f2718c560e27cc6f5a8b1e3e76
wangilamain <wangilayng@gmail.com>
2020-11-18 13:15:22 -0500

    images

--- migall/settings.py
+++ migall/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap4'
-    'gallery'
+    'bootstrap4',
+    'gallery',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
